8fbb6003b4343e3646281a6d3dea12e74c2b8529
connornm777 <connordol@gmail.com>
2025-07-25 17:07:24 -0400

    test

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
 
 
 
-x = torch.randn(32, 2,
-    device='cpu',
-    requires_grad=True)
-
-
+x = torch.randn(32, 2, device='gpu', requires_grad=True)
 
 
